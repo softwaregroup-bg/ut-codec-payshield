84dcf47585cdaead26ea1e6ba7c4f9dc9cef6340
zetxx <opensuser@gmail.com>
2014-12-09 19:36:49 +0200

    parser changes, few getters added, mocha test way changed

--- iso8583.js
+++ iso8583.js
@@ -15,15 +15,13 @@ module.exports = {
       bitsyntax.matcher('size:16/integer, msgType:4/binary, bitmap1:8/binary, bitmap2:8/binary, bitmap3:8/binary, rest/binary')
     ];
 
-    var bmpStrBuffer;
-    var mainStrBuffer;
-    var bmps = [];
-    var fields = [];
-    var size = 0;
-    var msgTypeID;
-
     var isoParser = function(buffer) {
       this.packet = buffer;
+      this.dataBuffer = '';
+      this.bitmaps = [];
+      this.fields = [];
+      this.size = -1;
+      this.typeID = '';//@TODO
     };
 
     /**
@@ -32,27 +30,55 @@ module.exports = {
      * @returns {void} [description]
      */
     isoParser.prototype.setBmps = function(bmp) {
+      this.bitmaps = [];
+
       for(var i in bmp) {
         if(i.indexOf('bitmap') !== -1) {
-          bmps.push(bmp[i])
+          this.bitmaps.push(bmp[i])
         }
       }
+    };
+
+    /**
+     * get bitmaps array.parseServiceData should be called before
+     * @returns {array} array of bitmaps in hex
+     */
+    isoParser.prototype.getBmps = function() {
+      return this.bitmaps;
+    };
+
+    /**
+     * get size of the message.parseServiceData should be called before
+     * @returns {integer} message size
+     */
+    isoParser.prototype.getSize = function() {
+      return this.size;
+    };
+
+    /**
+     * get message type id.parseServiceData should be called before
+     * @type {[type]}
+     */
+    isoParser.prototype.getTypeID = function() {
+      return this.typeID;
     };
 
     /**
      * extract bitmaps from buffer
      * @returns {void} [description]
      */
-    isoParser.prototype.extractBmps = function(count) {
+    isoParser.prototype.parseServiceData = function(count) {
       var _this = this;
       count = (!count?1:count);
       var _bmp = bitmasks[count-1](new Buffer(this.packet, 'hex'));
 
       if(_bmp['bitmap' + count.toString()][0]&128){//is there more bmps
-        return _this.extractBmps(count+1);
+        return _this.parseServiceData(count+1);
       } else {
         _this.setBmps(_bmp);
-        mainStrBuffer = _bmp.rest;
+        this.dataBuffer = _bmp.rest;
+        this.size = _bmp.size;
+        this.typeID = _bmp.msgType.toString();
       }
     };
 
@@ -65,7 +91,7 @@ module.exports = {
 
       for (var i = 8; i > 0; i--) {
         if(_byte&_s) {
-          fields.push(i+_plus);
+          this.fields.push(i+_plus);
         }
         _s = _s*2;
       };
@@ -76,26 +102,26 @@ module.exports = {
      * @type {[type]}
      */
     isoParser.prototype.parse = function() {
-      this.extractBmps();
+      this.parseServiceData();
 
-      for(var i in bmps) {
+      for(var i in this.bitmaps) {
         var _i = (parseInt(i)*64);
         for(var i2=0;i2 < 7;i2++) {
           var _i2 = parseInt(i2)*8;
-          this.findNumbers(bmps[i][i2], _i2+_i);
+          this.findNumbers(this.bitmaps[i][i2], _i2+_i);
         }
       }
 
-      fields = fields.sort(function(a, b) {return a-b;});
-      var fieldsPattern = this.getFieldsPattern();
-      console.log(fieldsPattern)
+      fields = this.fields.sort(function(a, b) {return a-b;});
+      var fieldsPattern = this.getFieldsPattern(fields);
+      //@TODO
     };
 
     /**
      * creates pattern for every parsed field
      * @returns {array} bitsyntax pattern
      */
-    isoParser.prototype.getFieldsPattern = function() {
+    isoParser.prototype.getFieldsPattern = function(fields) {
       var _fps = [];
       var _l = fields.length;
 
