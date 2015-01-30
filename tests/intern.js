fa44eef64e7e6b8f28b5e1bd21065db4786cb699
Martin Georgiev <martin.georgiev@softwaregroup-bg.com>
2015-01-30 13:04:23 +0200

    SqlPort

--- tests/intern.js
+++ tests/intern.js
@@ -3,6 +3,6 @@
  * If individual testing is required copy/paste intern.js with different name and change the command
  */
 define({
-	suites: ['tests/test.ndc.js', 'tests/test.smpp.js', 'tests/test.payshield.js'/*, 'tests/test.iso8583.js'*/],
+	suites: ['tests/test.ndc.js', 'tests/test.smpp.js',/* 'tests/test.payshield.js', 'tests/test.iso8583.js'*/],
 	excludeInstrumentation: /^(?:test)\//
 });
