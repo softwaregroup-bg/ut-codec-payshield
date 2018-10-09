d667a7150fd7c2257cd41985a572d9820f7c3a55
Krasimir Kolev <krasimir.kolev@softwaregroup.com>
2018-10-09 18:22:57 +0300

    fix: custom errors, minor fixes

--- errors/index.js
+++ errors/index.js
@@ -24,10 +24,19 @@ module.exports = (defineError) => {
     var defErr = messagesKeys.reduce((a1, c1) => {
         const MsgErr = defineError(c1, payshield, `Error in method: ${c1}`);
         a1[`${c1}.generic`] = defineError('generic', MsgErr, 'Generic Error');
-        return errorList.reduce((a2, c2) => {
-            a2[`${c1}.${c2}`] = defineError(c2, MsgErr, (messages[c1].customResponseError && messages[c1].customResponseError[c2]) || errorDesc[c2]);
+        var customResponseError = {};
+        if (messages[c1].customResponseError) {
+            customResponseError = Object.keys(messages[c1].customResponseError)
+                .map((key) => ({key, value: messages[c1].customResponseError[key]}))
+                .reduce((a, {key, value}) => (Object.assign(a, {
+                    [`${c1}.${key}`]: defineError(key, MsgErr, value)
+                })), {});
+        }
+        return Object.assign(errorList.reduce((a2, c2) => {
+            a2[`${c1}.${c2}`] = defineError(c2, MsgErr, errorDesc[c2]);
+            // a2[`${c1}.${c2}`] = defineError(c2, MsgErr, (messages[c1].customResponseError && messages[c1].customResponseError[c2]) || errorDesc[c2]);
             return a2;
-        }, a1);
+        }, a1), customResponseError);
     }, errors);
     return defErr;
 };
