50678309befb1f4d4baa0ea6eb0d982ed83cfa5c
elin.angelov <elin.angelov@softwaregroup-bg.com>
2018-10-10 10:14:08 +0300

    fix: errors

--- errors/index.js
+++ errors/index.js
@@ -18,22 +18,15 @@ module.exports = ({defineError, getError, fetchErrors}) => {
         defineError('notimplemented', Payshield, 'Not implemented opcode: {opcode}');
         defineError('unableMatchingPattern', Payshield, 'Unable to match pattern for opcode: {opcode}');
 
-    messagesKeys.reduce((a1, c1) => {
-        const MsgErr = defineError(c1, payshield, `Error in method: ${c1}`);
-        a1[`${c1}.generic`] = defineError('generic', MsgErr, 'Generic Error');
-        var customResponseError = {};
-        if (messages[c1].customResponseError) {
-            customResponseError = Object.keys(messages[c1].customResponseError)
-                .map((key) => ({key, value: messages[c1].customResponseError[key]}))
-                .reduce((a, {key, value}) => (Object.assign(a, {
-                    [`${c1}.${key}`]: defineError(key, MsgErr, value)
-                })), {});
-        }
-        return Object.assign(errorList.reduce((a2, c2) => {
-            a2[`${c1}.${c2}`] = defineError(c2, MsgErr, errorDesc[c2]);
-            return a2;
-        }, a1), customResponseError);
-    }, errors);
+        messagesKeys.map((method) => {
+            const MethodError = defineError(method, Payshield, `Error in method: ${method}`);
+            defineError(`${method}.generic`, MethodError, 'Generic Error');
+            // generate predefined list error
+            errorList.map((code) => defineError(code, MethodError, errorDesc[code]));
 
+            // generate custom per message error
+            messages[method].customResponseError && Object.keys(messages[method].customResponseError).map((code) => (defineError(code, MethodError, messages[method].customResponseError[code])));
+        });
+    }
     return fetchErrors('payshield');
 };
