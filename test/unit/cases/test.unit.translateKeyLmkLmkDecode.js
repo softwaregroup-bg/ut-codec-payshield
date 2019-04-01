9be6aed53e415025224f58c9a829f70c37ad6e73
Krasimir Kolev <krasimir.kolev@softwaregroup.com>
2019-04-01 10:40:01 +0300

    fix: tests

--- test/unit/cases/test.unit.translateKeyLmkLmkDecode.js
+++ test/unit/cases/test.unit.translateKeyLmkLmkDecode.js
@@ -6,7 +6,7 @@ const errorApi = { getError: get, fetchErrors: fetch, defineError: define };
 const Payshield = require('../../../index');
 const payshield = new Payshield(Object.assign({}, config, errorApi));
 
-const method = 'translateKeyLmk';
+const method = 'translateKeyLmkLmk';
 const testData = config.test[`${method}Decode`];
 const testResults = config.test[`${method}Decode`];
 const $meta = {
@@ -18,9 +18,9 @@ const context = {
     trace: 3
 };
 tap.test(`${method}Decode`, (t) => {
-    t.same(payshield.decode(testData[0].request, $meta, context), testResults[0].response, 'translateKeyLmk decode, success');
-    t.same(payshield.decode(testData[1].request, $meta, context).type, testResults[1].response, 'translateKeyLmk decode, LMK key change storage is corrupted');
-    t.same(payshield.decode(testData[2].request, $meta, context).type, testResults[2].response, 'translateKeyLmk decode, Key parity error');
+    t.same(payshield.decode(testData[0].request, $meta, context), testResults[0].response, 'translateKeyLmkLmk decode, success');
+    t.same(payshield.decode(testData[1].request, $meta, context).type, testResults[1].response, 'translateKeyLmkLmk decode, LMK key change storage is corrupted');
+    t.same(payshield.decode(testData[2].request, $meta, context).type, testResults[2].response, 'translateKeyLmkLmk decode, Key parity error');
 
     t.end();
 });
